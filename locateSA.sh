7115f3fe9a0e73ee861d420090e3076fdb2b0b54
Marcin Raniszewski <ranisz@gmail.com>
2017-12-18 08:52:31 +0100

    benchmark scripts added

---
+++ locateSA.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+make
+for q in 10000; do
+    for file in dna200; do
+        for m in 12 16 32 64; do
+            for type in std dbl lut2 lut2-dbl; do
+                sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
+                taskset -c 0 ./test/locateSA $type $file $q $m
+            done
+            for type in std-hash std-hash-dense dbl-hash dbl-hash-dense; do
+                for lf in 0.9; do
+                    sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
+                    taskset -c 0 ./test/locateSA $type 12 $lf $file $q $m
+                done
+            done
+        done
+    done
+done
+for q in 10000; do
+    for file in proteins200; do
+        for m in 5 16 32 64; do
+            for type in std dbl lut2 lut2-dbl; do
+                sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
+                taskset -c 0 ./test/locateSA $type $file $q $m
+            done
+            for type in std-hash std-hash-dense dbl-hash dbl-hash-dense; do
+                for lf in 0.9; do
+                    sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
+                    taskset -c 0 ./test/locateSA $type 5 $lf $file $q $m
+                done
+            done
+        done
+    done
+done
+for q in 10000; do
+    for file in english200 sources200 xml200; do
+        for m in 8 16 32 64; do
+            for type in std dbl lut2 lut2-dbl; do
+                sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
+                taskset -c 0 ./test/locateSA $type $file $q $m
+            done
+            for type in std-hash std-hash-dense dbl-hash dbl-hash-dense; do
+                for lf in 0.9; do
+                    sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
+                    taskset -c 0 ./test/locateSA $type 8 $lf $file $q $m
+                done
+            done
+        done
+    done
+done
